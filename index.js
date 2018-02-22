5a938ed2c55e988a2a7639619be0d95952185c3f
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2018-02-22 16:28:39 +0100

    background + pushspin

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
-    $('.pushpin-demo-nav').pushpin({
-        top: 0,
-        bottom: 1000,
-        offset: 0
+$('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
     });
 });
