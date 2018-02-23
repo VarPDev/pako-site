b884a78a3c01e4ce7c190f3a750905e30b650ce7
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2018-02-23 14:33:49 +0100

    boh

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-$('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-        top: $target.offset().top,
-        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+function pushspin() {
+    $('.pushpin-demo-nav').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+            top: $target.offset().top,
+            bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
     });
+}
+
+$( window ).resize(function() {
+    pushspin();
+});
+
+$( document ).ready(function() {
+    pushspin();
 });
 
 var options = [
-    {selector: '#blue', offset: 300, callback: function(el) {
+    {selector: '#blue', offset: 1500, callback: function(el) {
         Materialize.toast("Surprised!? Don't worry be happy!", 3000 );
     } },
-    {selector: '#green', offset: 500, callback: function(el) {
+    {selector: '#green', offset: 1000, callback: function(el) {
         Materialize.toast("Contac me!", 2000 );
     } }
   ];
